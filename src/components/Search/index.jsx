6e6faa814e2923010b58910ba88008abb777026b
z-takaev <zatakata.web@gmail.com>
2022-05-25 21:44:43 +0300

    add icons for search component and add clear input

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -8,6 +8,9 @@ function Search() {
 
   return (
     <div className={styles.root}>
+      <svg className={styles.searchIcon} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+      </svg>
       <input
         className={styles.input}
         type="text"
@@ -15,6 +18,16 @@ function Search() {
         value={searchValue}
         onChange={(event) => setSearchValue(event.target.value)}
       />
+      {searchValue && (
+        <svg
+          className={styles.searchClose}
+          onClick={() => setSearchValue('')}
+          viewBox="0 0 48 48"
+          xmlns="http://www.w3.org/2000/svg">
+          <path d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z" />
+          <path d="M0 0h48v48h-48z" fill="none" />
+        </svg>
+      )}
     </div>
   );
 }
